3f57b0017be55b84ce23c3139db7f2d44dec3f1d
Marthejansonskogen <Marthejansonskogen@192.168.0.25>
2017-05-17 22:49:37 +0200

    Merge origin/master
    Conflicts:
    	client/components/user_page.jsx
    	server/server.js

--- client/actions/menu_action.js
+++ client/actions/menu_action.js
@@ -1,19 +1,24 @@
 
 export const LIST_VIEW = 'LIST_VIEW';
 
-
+export function setDefaultPeople() {
+    return {
+        type: LIST_VIEW,
+        payload: 'default'
+    };
+}
 export function setListView() {
     return {
         type: LIST_VIEW,
         payload: 'list'
-    }
+    };
 }
 
 export function setGridView() {
     return {
         type: LIST_VIEW,
         payload: 'grid'
-    }
+    };
 }
 
 
