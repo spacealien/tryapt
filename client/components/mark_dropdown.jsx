6c205d3bf3e592b596462ef32c293a20676bc651
Odd <odd.einar.hoel@gmail.com>
2017-05-17 22:07:58 +0200

    merge

--- client/components/mark_dropdown.jsx
+++ client/components/mark_dropdown.jsx
@@ -5,15 +5,15 @@
  */
 import React from 'react';
 
-class MarkDropdown extends React.Component {
+class Dropdown extends React.Component {
     constructor(props) {
         super(props);
 
         this.state = {
             visible: false,
 
-            isAllMarked: false,
-            markAllOptionText: 'Marker Alle'
+            isAlled: false,
+            AllOptionText: 'er Alle'
 
         };
 
@@ -23,48 +23,49 @@ class MarkDropdown extends React.Component {
 
     toggle() {
         if (!this.state.visible) {
-            document.getElementById('markDropdown').style.display = 'block';
+            document.getElementById('Dropdown').style.display = 'block';
         }
         else {
-            document.getElementById('markDropdown').style.display = 'none';
+            document.getElementById('Dropdown').style.display = 'none';
         }
         this.setState({
             visible: !this.state.visible
         });
     }
 
-    toggleMarkAll() {
-        this.props.onToggleMarkAll(this.state.isAllMarked);
-        this.setState({ isAllMarked: !this.state.isAllMarked });
-        if (this.state.markAllOptionText == 'Marker Alle') {
-            this.setState({ markAllOptionText: 'Marker Ingen' });
+    toggleAll() {
+        this.props.onToggleAll(this.state.isAlled);
+        this.setState({ isAlled: !this.state.isAlled });
+        if (this.state.AllOptionText == 'er Alle') {
+            this.setState({ AllOptionText: 'er Ingen' });
         } else {
-            this.setState({ markAllOptionText: 'Marker Alle' });
+            this.setState({ AllOptionText: 'er Alle' });
         }
     }
 
 
     render() {
-        const { markAllOptionText } = this.state;
+        const { AllOptionText } = this.state;
 
         return (
-            <div className="mark-dropdown"  >
+            <div className="-dropdown"  >
 
                 <img
-                    className='mark-dropimg'
+                    className='-dropimg'
                     src='https://cdn1.iconfinder.com/data/icons/pixel-perfect-at-16px-volume-2/16/5001-128.png'
                     onClick={() => this.toggle()} />
 
-                <div id='markDropdown' className='mark-dropdown-content'>
+                <div id='Dropdown' className='-dropdown-content'>
 
                     <div
-                        className='mark-dropdown-element'
-                        onClick={() => this.toggleMarkAll()}>{markAllOptionText}</div>
+                        className='-dropdown-element'
+                        onClick={() => this.toggleAll()}>{AllOptionText}</div>
 
-                    <div className='mark-dropdown-element'>Send til markerte<img src='https://cdn3.iconfinder.com/data/icons/google-material-design-icons/48/ic_keyboard_arrow_right_48px-64.png' /> </div>
+                    <div className='-dropdown-element'
+                    onClick={() => this.props.sendToMarked()}>Send til erte<img src='https://cdn3.iconfinder.com/data/icons/google-material-design-icons/48/ic_keyboard_arrow_right_48px-64.png' /> </div>
                 </div>
             </div>
         );
     }
 }
-export default MarkDropdown;
+export default Dropdown;
