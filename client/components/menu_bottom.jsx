4489e2dc828a560b037ddbfadb478d6a00003569
Odd <odd.einar.hoel@hotmail.com>
2017-05-18 13:41:32 +0200

    grid/view buttom menu farge helvete og mark_Dropdown merge lok

--- client/components/menu_bottom.jsx
+++ client/components/menu_bottom.jsx
@@ -7,6 +7,9 @@ import { connect } from 'react-redux';
 class Menu extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            active: ''
+        }
     }
 
     handleClick(e) {
@@ -14,50 +17,56 @@ class Menu extends React.Component {
 
         switch (id) {
             case 'home':
-                document.getElementById(id).style.color="#D1A25F";
-                document.getElementById('login').style.color="#333333";
-                document.getElementById('people').style.color="#333333";
-                document.getElementById('info').style.color="#333333";
+                document.getElementById(id).style.color = "#D1A25F";
+                document.getElementById('login').style.color = "#333333";
+                document.getElementById('people').style.color = "#333333";
+                document.getElementById('info').style.color = "#333333";
                 // this.props.setDefaultPeople();
                 this.props.browserHistory.push('/');
+                this.setState({ active: 'home' });
                 break;
             case "login":
-                document.getElementById(id).style.color="#D1A25F";
-                document.getElementById('home').style.color="#333333";
-                document.getElementById('people').style.color="#333333";
-                document.getElementById('info').style.color="#333333";
+                document.getElementById(id).style.color = "#D1A25F";
+                document.getElementById('home').style.color = "#333333";
+                document.getElementById('people').style.color = "#333333";
+                document.getElementById('info').style.color = "#333333";
                 this.props.browserHistory.push('/my_page');
+                this.setState({ active: 'login' });
                 break;
             case "people":
-                
-                
-                document.getElementById('login').style.color="#333333";
-                document.getElementById('home').style.color="#333333";
-                document.getElementById('info').style.color="#333333";
-                if(this.props.listView==='list') {
+                console.log('people click');
+                this.setState({ active: '' });
+                document.getElementById('login').style.color = "#333333";
+                document.getElementById('home').style.color = "#333333";
+                document.getElementById('info').style.color = "#333333";
+                if (this.props.listView === 'list') {
                     this.props.setGridView();
                 } else {
                     this.props.setListView();
                 }
-                
                 this.props.browserHistory.push('/people');
-                document.getElementById('people').style.color="#D1A25F";
-                
+               // document.getElementById('people').style.color = "#D1A25F";
+                this.setState({ active: 'people' });
+
                 break;
             case 'info':
-                document.getElementById(id).style.color="#D1A25F";
-                document.getElementById('login').style.color="#333333";
-                document.getElementById('people').style.color="#333333";
-                document.getElementById('home').style.color="#333333";
+                document.getElementById(id).style.color = "#D1A25F";
+                document.getElementById('login').style.color = "#333333";
+                document.getElementById('people').style.color = "#333333";
+                document.getElementById('home').style.color = "#333333";
                 this.props.browserHistory.push('/info');
+                this.setState({ active: 'info' });
                 break;
         }
     }
 
     render() {
-
         const { listView } = this.props;
+        const { active } = this.state;
 
+        
+        var peopleStyle = this.state.active === 'people' ? { color: "#D1A25F"} : { color: "#333333" };
+        console.log(peopleStyle);
 
         return (
             <footer>
@@ -73,19 +82,21 @@ class Menu extends React.Component {
 
                             <div id="listgrid" className="col-sm-3 menu-bottom-cl">
                                 <button className="btn menu-bottom" onClick={(e) => this.handleClick(e)}>
-                                    {listView==='list' && <i id="people" className="material-icons icons-menu-bottom grid-icon">view_module</i>} 
-                                    {listView==='grid' && <i id="people" className="material-icons icons-menu-bottom">view_list</i>} 
+                                    {listView === 'list' &&
+                                        <i id="people" className="material-icons icons-menu-bottom grid-icon" style={peopleStyle}> view_module</i>}
+                                    {listView === 'grid'
+                                        && <i id="people" className="material-icons icons-menu-bottom" style={peopleStyle}> view_list</i>}
                                 </button>
                             </div>
 
                             <div className="col-sm-3 menu-bottom-cl">
-                                    <button className="btn menu-botton" onClick={(e) => this.handleClick(e)}>
-                                        <i id="login" className="material-icons icons-menu-bottom">account_box</i>
-                                    </button>
+                                <button className="btn menu-botton" onClick={(e) => this.handleClick(e)}>
+                                    <i id="login" className="material-icons icons-menu-bottom">account_box</i>
+                                </button>
                             </div>
 
                             <div className="col-sm-3 menu-bottom-cl">
-                                
+
                                 <button className="btn menu-botton" onClick={(e) => this.handleClick(e)}>
                                     <i id="info" className="material-icons icons-menu-bottom">info</i>
                                 </button>
