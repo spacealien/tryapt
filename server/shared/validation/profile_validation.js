6fcc2d201e6cff8413d5c607dc3074e326150c52
Odd <odd.einar.hoel@gmail.com>
2017-05-24 07:41:36 +0200

    lit dritt

--- server/shared/validation/profile_validation.js
+++ server/shared/validation/profile_validation.js
@@ -4,15 +4,6 @@ import _ from 'underscore';
 export default function profileValidator(data) {
     let errors = {};
 
-    // checks is the email and password is empty
-    if (data.linked !== null && Validator.isEmpty(data.email)) {
-        errors.email = 'Dette feltet er påkrevd';
-    }
-
-    // checks is the email and password is empty
-    if (data.experience !== null && Validator.isEmpty(data.email)) {
-        errors.email = 'Dette feltet er påkrevd';
-    }
 
     return { errors, isValid: _.isEmpty(errors) };
 }
